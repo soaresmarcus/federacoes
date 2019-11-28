1ecf75cba51471228f277a1a924018a015ea212d
Marcus Soares <marcus.soares@sisqualis.com.br>
2019-11-28 20:23:49 -0300

    Commit inicial

---
+++ src/main/java/com/avaliacao/federacoes/FederacoesApplication.java
@@ -0,0 +1,13 @@
+package com.avaliacao.federacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FederacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FederacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avaliacao/federacoes/FederacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avaliacao.federacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FederacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
